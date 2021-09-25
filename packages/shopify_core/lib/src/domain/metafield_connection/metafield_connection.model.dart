3de6fa5ab81d0edd6f04ce189f3456f91bdd1b0c
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-25 10:31:09 -0500

    feat: create metafield connection

---
+++ packages/shopify_core/lib/src/domain/metafield_connection/metafield_connection.model.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:shopify_core/src/domain/metafield_edge/metafield_edge.model.dart';
+import 'package:shopify_core/src/domain/page_info/page_info.model.dart';
+
+part 'metafield_connection.model.freezed.dart';
+part 'metafield_connection.model.g.dart';
+
+/// An auto-generated type for paginating through multiple Metafields
+@freezed
+class MetafieldConnection with _$MetafieldConnection {
+  const MetafieldConnection._();
+
+  const factory MetafieldConnection({
+    /// A list of edges.
+    @Default([]) List<MetafieldEdge> edges,
+
+    /// Information to aid in pagination.
+    required PageInfo pageInfo,
+  }) = _MetafieldConnection;
+
+  factory MetafieldConnection.fromJson(Map<String, dynamic> json) =>
+      _$MetafieldConnectionFromJson(json);
+}
