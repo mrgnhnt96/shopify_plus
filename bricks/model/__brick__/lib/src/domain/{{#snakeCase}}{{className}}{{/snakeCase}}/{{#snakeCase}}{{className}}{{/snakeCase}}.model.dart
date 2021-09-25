5064fe673d757bd579b95ca3f4863162b660201e
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-25 17:43:02 -0500

    refactor: Update bricks

--- bricks/model/__brick__/lib/src/domain/{{#snakeCase}}{{className}}{{/snakeCase}}/{{#snakeCase}}{{className}}{{/snakeCase}}.model.dart
+++ bricks/model/__brick__/lib/src/domain/{{#snakeCase}}{{className}}{{/snakeCase}}/{{#snakeCase}}{{className}}{{/snakeCase}}.model.dart
@@ -4,12 +4,17 @@ part '{{#snakeCase}}{{className}}{{/snakeCase}}.model.freezed.dart';
 part '{{#snakeCase}}{{className}}{{/snakeCase}}.model.g.dart';
 
 // TODO: Add properties, constructors, and methods
+
+/// {@template {{#snakeCase}}{{className}}{{/snakeCase}}}
+/// {@endtemplate}
 @freezed
 class {{#pascalCase}}{{className}}{{/pascalCase}} with _${{#pascalCase}}{{className}}{{/pascalCase}}  {
   const {{#pascalCase}}{{className}}{{/pascalCase}}._();
 
+  /// {@macro {{#snakeCase}}{{className}}{{/snakeCase}}}
   const factory {{#pascalCase}}{{className}}{{/pascalCase}}() = _{{#pascalCase}}{{className}}{{/pascalCase}};
 
+  /// {@macro from_json}
   factory {{#pascalCase}}{{className}}{{/pascalCase}}.fromJson(Map<String, dynamic> json) =>
       _${{#pascalCase}}{{className}}{{/pascalCase}}FromJson(json);
 
