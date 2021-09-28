5fbdbe3aebbd7a2885073635f040cab9dcff52f1
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-28 08:34:43 -0500

    feat: create error brick

---
+++ bricks/error/__brick__/lib/src/error/{{#snakeCase}}{{className}}{{/snakeCase}}/{{#snakeCase}}{{className}}{{/snakeCase}}_error.model.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '{{#snakeCase}}{{className}}{{/snakeCase}}_error.model.freezed.dart';
+part '{{#snakeCase}}{{className}}{{/snakeCase}}_error.model.g.dart';
+
+// TODO: Add properties, constructors, and methods
+
+/// {@template {{#snakeCase}}{{className}}{{/snakeCase}}_error}
+/// {@endtemplate}
+@freezed
+class {{#pascalCase}}{{className}}{{/pascalCase}}Error with _${{#pascalCase}}{{className}}{{/pascalCase}}Error  {
+  const {{#pascalCase}}{{className}}{{/pascalCase}}Error._();
+
+  /// {@macro {{#snakeCase}}{{className}}{{/snakeCase}}_error}
+  const factory {{#pascalCase}}{{className}}{{/pascalCase}}Error() = _{{#pascalCase}}{{className}}{{/pascalCase}}Error;
+
+  /// {@macro from_json}
+  factory {{#pascalCase}}{{className}}{{/pascalCase}}Error.fromJson(Map<String, dynamic> json) =>
+      _${{#pascalCase}}{{className}}{{/pascalCase}}ErrorFromJson(json);
+
+}
