5f90178e8038b2b5826f58f93e10949ae59778f1
Morgan Hunt <mrgnhnt96@gmail.com>
2021-09-28 19:55:53 -0500

    refactor(error): Update barrel file

--- packages/shopify_storefront_core/lib/src/error/error.dart
+++ packages/shopify_storefront_core/lib/src/error/error.dart
@@ -1,2 +1,4 @@
 export 'cart_user/cart_user_error.model.dart';
 export 'checkout_user/checkout_user_error.model.dart';
+export 'customer_user/customer_user_error.model.dart';
+export 'user/user_error.model.dart';
